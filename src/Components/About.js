dce1a433b78ef436ccb4ff760b61eb125e3328fb
shalekataye <shalekataye07@gmail.com>
2021-12-06 15:01:43 +0300

    first commit

--- src/Components/About.js
+++ src/Components/About.js
@@ -14,7 +14,7 @@ class About extends Component {
     const zip = this.props.data.address.zip;
     const phone = this.props.data.phone;
     const email = this.props.data.email;
-    const resumeDownload = this.props.data.resumedownload;
+ 
 
     return (
       <section id="about">
--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -23,11 +23,11 @@ class Footer extends Component {
               <ul className="social-links">{networks}</ul>
 
               <ul className="copyright">
-                <li>&copy; Copyright 2021 Nordic Giant</li>
+                <li>&copy; Copyright 2021 Shaleka Taye </li>
                 <li>
                   Design by{" "}
-                  <a title="Styleshout" href="http://www.styleshout.com/">
-                    Styleshout
+                  <a title="Styleshout" href="https://github.com/shalekataye">
+                    React js 
                   </a>
                 </li>
               </ul>
